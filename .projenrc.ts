23530b55936c3912a1daa2832b9615255543c1ab
Court Schuett <schuettc@amazon.com>
2023-12-06 10:05:37 -0600

    fix: update schedule

--- .projenrc.ts
+++ .projenrc.ts
@@ -11,12 +11,14 @@ const project = new awscdk.AwsCdkTypeScriptApp({
   author: 'Court Schuett',
   copyrightOwner: 'Amazon.com, Inc.',
   authorAddress: 'https://aws.amazon.com',
+  jest: false,
   projenrcTs: true,
   appEntrypoint: 'amazon-chime-sdk-take-back-and-transfer.ts',
   depsUpgradeOptions: {
     ignoreProjen: false,
     workflowOptions: {
       labels: ['auto-approve', 'auto-merge'],
+      schedule: UpgradeDependenciesSchedule.WEEKLY,
     },
   },
   autoApproveOptions: {
@@ -33,53 +35,6 @@ const project = new awscdk.AwsCdkTypeScriptApp({
   ],
   autoApproveUpgrades: true,
   projenUpgradeSecret: 'PROJEN_GITHUB_TOKEN',
-});
-
-const upgradeSite = project.github.addWorkflow('upgrade-site');
-upgradeSite.on({ schedule: [{ cron: '0 5 * * 1' }], workflowDispatch: {} });
-upgradeSite.addJobs({
-  upgradeSite: {
-    runsOn: ['ubuntu-latest'],
-    name: 'upgrade-site',
-    permissions: {
-      actions: JobPermission.WRITE,
-      contents: JobPermission.READ,
-      idToken: JobPermission.WRITE,
-    },
-    steps: [
-      { uses: 'actions/checkout@v3' },
-      {
-        name: 'Setup Node.js',
-        uses: 'actions/setup-node@v3',
-        with: {
-          'node-version': '18',
-        },
-      },
-      {
-        run: 'yarn install --check-files --frozen-lockfile',
-        workingDirectory: 'src/resources/server/assets/site',
-      },
-      {
-        run: 'yarn upgrade',
-        workingDirectory: 'src/resources/server/assets/site',
-      },
-      {
-        name: 'Create Pull Request',
-        uses: 'peter-evans/create-pull-request@v4',
-        with: {
-          'token': '${{ secrets.' + AUTOMATION_TOKEN + ' }}',
-          'commit-message': 'chore: upgrade site',
-          'branch': 'auto/projen-upgrade',
-          'title': 'chore: upgrade site',
-          'body': 'This PR upgrades site',
-          'labels': 'auto-merge, auto-approve',
-          'author': 'github-actions <github-actions@github.com>',
-          'committer': 'github-actions <github-actions@github.com>',
-          'signoff': true,
-        },
-      },
-    ],
-  },
 });
 
 const common_exclude = [
